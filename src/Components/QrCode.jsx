ceffb70477fa29b0c3cfe21884cadb3c6f5f00b7
Ravi16Patidar <ravi4116patidar@gmail.com>
2023-10-13 17:07:22 +0530

    qr-generator

--- src/Components/QrCode.jsx
+++ src/Components/QrCode.jsx
@@ -1,11 +1,24 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
-import QRcode from 'react-qr-code'
+// import QRcode from 'react-qr-code'
+import QRcode from 'qrcode.react'
 import CancelIcon from '@mui/icons-material/Cancel';
 export default function QrCode2() {
     const [inputData,setinputData]=useState("Create Your Own QR Code")
     const [qrValue,setQrValue]=useState("");
+    const [url,setUrl]=useState("");
+
+    // download function 
+    const downloadQR = () => {
+    
+      // let downloadLink = document.createElement("a");
+      // downloadLink.href = pngUrl;
+      // downloadLink.download = "myqr.png";
+      // document.body.appendChild(downloadLink);
+      // downloadLink.click();
+      // document.body.removeChild(downloadLink);
+  };
 
     function handleInput(e)
     {
@@ -15,12 +28,19 @@ export default function QrCode2() {
     {
         setQrValue(inputData)
         setinputData("")
+
+        const canvas = document.getElementById("myqr");
+        const pngUrl = canvas
+          .toDataURL("image/png")
+           setUrl(pngUrl)
     }
     function clearInput()
     {
       setinputData("")
     }
-
+  useEffect(()=>{
+    
+  },[qrValue])
   return (
     <div className='flex'>
       <h1 className='styleHeading'>URL TO <span>QR CODE</span></h1>
@@ -31,9 +51,20 @@ export default function QrCode2() {
       <div>
       <Button variant="contained" onClick={qrCode}>GENRRATE QR CODE</Button>
       </div>
-      <div>
-        <QRcode value={qrValue} className='qr-code' />
+      <div>  
+        <QRcode value={qrValue} className='qr-code' id="myqr"/>
         </div>
+        <div>
+      
+      <Button variant="contained"> <a target='_blank'
+        href={url}
+        style={{color:"white",textDecoration:"none"}}
+        download
+   
+      >
+        Download
+      </a></Button>
+      </div>
     </div>
   )
 }

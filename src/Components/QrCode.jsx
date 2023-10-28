81370be2773286ee89a017d3e3f5861aecb08f88
Ravi16Patidar <ravi4116patidar@gmail.com>
2023-10-28 18:58:56 +0530

    qr-generator

--- src/Components/QrCode.jsx
+++ src/Components/QrCode.jsx
@@ -1,63 +1,93 @@
-import React, { useEffect, useState } from 'react'
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
-import QRcode from 'qrcode.react'
-import CancelIcon from '@mui/icons-material/Cancel';
+import React, { useEffect, useState } from "react";
+import TextField from "@mui/material/TextField";
+import { Snackbar, Button } from "@mui/material";
+import Alert from '@mui/material/Alert';
+
+import QRcode from "qrcode.react";
+import CancelIcon from "@mui/icons-material/Cancel";
 export default function QrCode2() {
-    const [inputData,setinputData]=useState("Create Your Own QR Code")
-    const [qrValue,setQrValue]=useState("");
-    const [url,setUrl]=useState("");
+  const [open, setOpen] = useState(false);
+  const [inputData, setinputData] = useState("Create Your Own QR Code");
+  const [qrValue, setQrValue] = useState("");
+  const [url, setUrl] = useState("");
 
-    const downloadQR = () => {
-    
+  const downloadQR = () => {};
 
-  };
+  function handleInput(e) {
+    setinputData(e.target.value);
+  }
+  function qrCode() {
+    setQrValue(inputData);
+    setinputData("");
 
-    function handleInput(e)
-    {
-        setinputData(e.target.value)
+    const canvas = document.getElementById("myqr");
+    const pngUrl = canvas.toDataURL("image/png");
+    setUrl(pngUrl);
+  }
+  function clearInput() {
+    setinputData("");
+  }
+  function copyUrl() {
+    setOpen(true);
+    navigator.clipboard.writeText(inputData);
+  }
+  function handleSnackbarClose(event, reason) {
+    if (reason === "clickaway") {
+      return;
     }
-    function qrCode()
-    {
-        setQrValue(inputData)
-        setinputData("")
-
-        const canvas = document.getElementById("myqr");
-        const pngUrl = canvas
-          .toDataURL("image/png")
-           setUrl(pngUrl)
-    }
-    function clearInput()
-    {
-      setinputData("")
-    }
-  useEffect(()=>{
-    
-  },[qrValue])
+    setOpen(false);
+  }
+  useEffect(() => {}, [qrValue]);
   return (
-    <div className='flex'>
-      <h1 className='styleHeading'>URL TO <span>QR CODE</span></h1>
-    <div>
-      <TextField id="outlined-basic" label="URL" value={inputData}  variant="outlined" onChange={handleInput} className='input-width' autoComplete="off"></TextField>
-      <CancelIcon onClick={clearInput} className='clearInput'/>
+    <div className="flex">
+      <h1 className="styleHeading">
+        URL TO <span>QR CODE</span>
+      </h1>
+      <div>
+        <TextField
+          id="outlined-basic"
+          label="URL"
+          value={inputData}
+          variant="outlined"
+          onChange={handleInput}
+          className="input-width"
+          autoComplete="off"
+        ></TextField>
+        <CancelIcon onClick={clearInput} className="clearInput" />
+      </div>
+      <div style={{ display: "flex", gap: "120px", margin: "15px 0" }}>
+        <Button variant="contained" sx={{ width: "200px" }} onClick={copyUrl}>
+          Copy URL
+        </Button>
+        <Button variant="contained" onClick={qrCode} sx={{ width: "200px" }}>
+          GENRRATE QR CODE
+        </Button>
+        <Snackbar open={open} autoHideDuration={6000} onClose={handleSnackbarClose}>
+          <Alert
+            onClose={handleSnackbarClose}
+            severity="success"
+            sx={{ width: "100%" }}
+          >
+            This is a success message!
+          </Alert>
+        </Snackbar>
       </div>
       <div>
-      <Button variant="contained" onClick={qrCode}>GENRRATE QR CODE</Button>
+        <QRcode value={qrValue} className="qr-code" id="myqr" />
       </div>
-      <div>  
-        <QRcode value={qrValue} className='qr-code' id="myqr"/>
-        </div>
-        <div>
-      
-      <Button variant="contained"> <a target='_blank'
-        href={url}
-        style={{color:"white",textDecoration:"none"}}
-        download
-   
-      >
-        Download
-      </a></Button>
+      <div>
+        <Button variant="contained">
+          {" "}
+          <a
+            target="_blank"
+            href={url}
+            style={{ color: "white", textDecoration: "none" }}
+            download
+          >
+            Download
+          </a>
+        </Button>
       </div>
     </div>
-  )
+  );
 }
